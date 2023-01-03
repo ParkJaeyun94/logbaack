eda45cec8df130c5b12012c69ddfee944a2f84e5
jaeyun <ParkjaeYun>
2023-01-03 11:28:44 +0900

    Initial commit

---
+++ src/main/java/dev/be/logbaack/LogbaackApplication.java
@@ -0,0 +1,13 @@
+package dev.be.logbaack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbaackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogbaackApplication.class, args);
+    }
+
+}
